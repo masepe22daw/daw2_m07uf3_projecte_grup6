e8d9bd452faec939fd6763adc05fe27e331b4598
masepe <15585133.clot@fje.edu>
2023-05-16 21:39:32 +0200

    edita con bootstrap

--- resources/views/investigador/edit.blade.php
+++ resources/views/investigador/edit.blade.php
@@ -1,15 +1,27 @@
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <title>Crea Investigadors</title>
+</head>
+<style>
+        .content-container {
+            margin-left: 20px;
+        }
+    </style>
+<div class="content-container">
 <form method="POST" action="{{ route('investigador.update') }}">
     @csrf
     @method('PUT')
 
-    <div>
-        <label for="passaport">Passaport:</label>
-        <input type="text" name="passaport" id="passaport">
+    <div class="mb-3">
+        <label for="passaport" class="form-label">Passaport:</label>
+        <input type="text" name="passaport" id="passaport" class="form-control">
     </div>
 
-    <div>
-        <label for="campo">Campo a editar:</label>
-        <select name="campo" id="campo">
+    <div class="mb-3">
+        <label for="campo" class="form-label">Campo a editar:</label>
+        <select name="campo" id="campo" class="form-select">
             <option value="Passaport">Passaport</option>
             <option value="NomCognoms">Nom i Cognoms</option>
             <option value="CodiAssegMèdica">CodiAssegMèdica</option>
@@ -23,21 +35,23 @@
         </select>
     </div>
 
-    <div>
-        <label for="valor">Nuevo valor:</label>
-        <input type="text" name="valor" id="valor" >
+    <div class="mb-3">
+        <label for="valor" class="form-label">Nuevo valor:</label>
+        <input type="text" name="valor" id="valor" class="form-control">
     </div>
 
     <div>
-        <button type="submit">Actualizar</button>
+        <button type="submit" class="btn btn-primary">Actualizar</button>
     </div>
 </form>
 
 @if (session('success'))
-        <div class="alert alert-success">
-            {{ session('success') }}
-        </div>
-    @endif
+    <div class="alert alert-success">
+        {{ session('success') }}
+    </div>
+@endif
+
 <div>
-    <a href="{{ route('investigador.index') }}" class="btn btn-secondary"><button>Torna enrere</button></a>
+    <a href="{{ route('investigador.index') }}" class="btn btn-secondary">Torna enrere</a>
 </div>
+    </div>
